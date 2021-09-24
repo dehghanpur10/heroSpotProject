2c5dbf384e9a3c767fb157364b92b187525cd717
Mohammad Dehghanpour <m.dehghanpour10@gmail.com>
2021-09-24 21:31:17 +0330

    add integration test for createVehicleContorller

--- controller/vehicle/create_test.go
+++ controller/vehicle/create_test.go
@@ -8,13 +8,17 @@ import (
 	"github.com/stretchr/testify/require"
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"spotHeroProject/lib"
 	"spotHeroProject/models"
 	"spotHeroProject/service/vehicleService"
 	"testing"
 )
 
-func TestCreateVehicleController(t *testing.T) {
+func TestCreateVehicleControllerSuccess(t *testing.T) {
+	err := os.Setenv("AWS_REGION", "us-west-2")
+	require.NoError(t, err)
+
 	vehicle := models.Vehicle{
 		Id: "123456",
 		Description: models.VehicleDescription{
@@ -23,45 +27,94 @@ func TestCreateVehicleController(t *testing.T) {
 			Model: "55",
 		},
 	}
-	tests := []struct {
-		name   string
-		input  models.Vehicle
-		status int
-		output interface{}
-	}{
-		{name: "ok", input: vehicle, status: 201, output: vehicle},
-	}
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			// Arrange
-			router := mux.NewRouter()
-			router.HandleFunc("/v2/vehicle", CreateVehicleController).Methods("POST")
-			marshal, err := json.Marshal(test.input)
-			require.NoError(t, err)
-			req, _ := http.NewRequest(http.MethodPost, "/v2/vehicle", bytes.NewBuffer(marshal))
-			rr := httptest.NewRecorder()
-			// Act
-			router.ServeHTTP(rr, req)
-			//Assert
-			assert.Equal(t, test.status, rr.Code)
-			switch rr.Code {
-			case 201:
-				var vehicle models.Vehicle
-				err = json.Unmarshal(rr.Body.Bytes(), &vehicle)
-				require.NoError(t, err)
-				assert.Equal(t, test.output.(models.Vehicle), vehicle)
-			default:
-				var errorResponse lib.ErrorResponse
-				err = json.Unmarshal(rr.Body.Bytes(), &errorResponse)
-				require.NoError(t, err)
-				assert.Equal(t, test.output.(lib.ErrorResponse), errorResponse)
-			}
+	expectedStatus := 201
+	router := mux.NewRouter()
+	router.HandleFunc("/v2/vehicle", CreateVehicleController).Methods("POST")
+	marshal, err := json.Marshal(vehicle)
+	require.NoError(t, err)
+	req, _ := http.NewRequest(http.MethodPost, "/v2/vehicle", bytes.NewBuffer(marshal))
+	rr := httptest.NewRecorder()
+	// Act
+	router.ServeHTTP(rr, req)
+	//Assert
+	assert.Equal(t, expectedStatus, rr.Code)
+	var output models.Vehicle
+	err = json.Unmarshal(rr.Body.Bytes(), &output)
+	require.NoError(t, err)
+	assert.Equal(t, vehicle, output)
 
-		})
-	}
 	db, err := lib.GetDynamoDB()
 	require.NoError(t, err)
 	service := vehicleService.New(db)
 	err = service.DeleteVehicle(vehicle.Id)
 	require.NoError(t, err)
+	err = os.Unsetenv("AWS_REGION")
+	require.NoError(t, err)
 }
+
+func TestCreateVehicleControllerFailOnDecode(t *testing.T) {
+	expectedStatus := 400
+	expectedError := lib.ErrorResponse{Code: 400, Title: "Bad request", Description: "please enter correct body request"}
+	router := mux.NewRouter()
+	router.HandleFunc("/v2/vehicle", CreateVehicleController).Methods("POST")
+	marshal, err := json.Marshal("")
+	require.NoError(t, err)
+	req, _ := http.NewRequest(http.MethodPost, "/v2/vehicle", bytes.NewBuffer(marshal))
+	rr := httptest.NewRecorder()
+	// Act
+	router.ServeHTTP(rr, req)
+	//Assert
+	assert.Equal(t, expectedStatus, rr.Code)
+	var errorResponse lib.ErrorResponse
+	err = json.Unmarshal(rr.Body.Bytes(), &errorResponse)
+	require.NoError(t, err)
+	assert.Equal(t, expectedError, errorResponse)
+
+}
+func TestCreateVehicleControllerFailOnValidate(t *testing.T) {
+	expectedStatus := 400
+	expectedError := lib.ErrorResponse{Code: 400, Title: "Bad request", Description: "all fields should be send"}
+	router := mux.NewRouter()
+	router.HandleFunc("/v2/vehicle", CreateVehicleController).Methods("POST")
+	marshal, err := json.Marshal(models.Vehicle{})
+	require.NoError(t, err)
+	req, _ := http.NewRequest(http.MethodPost, "/v2/vehicle", bytes.NewBuffer(marshal))
+	rr := httptest.NewRecorder()
+	// Act
+	router.ServeHTTP(rr, req)
+	//Assert
+	assert.Equal(t, expectedStatus, rr.Code)
+	var errorResponse lib.ErrorResponse
+	err = json.Unmarshal(rr.Body.Bytes(), &errorResponse)
+	require.NoError(t, err)
+	assert.Equal(t, expectedError, errorResponse)
+
+}
+
+func TestCreateVehicleControllerFailOnGetDynamodb(t *testing.T) {
+	vehicle := models.Vehicle{
+		Id: "123456",
+		Description: models.VehicleDescription{
+			Name:  "vehicle1",
+			Year:  "5",
+			Model: "55",
+		},
+	}
+	expectedStatus := 500
+	expectedError := lib.ErrorResponse{Code: 500, Title: "Internal error", Description: "Internal server error."}
+	router := mux.NewRouter()
+	router.HandleFunc("/v2/vehicle", CreateVehicleController).Methods("POST")
+	marshal, err := json.Marshal(vehicle)
+	require.NoError(t, err)
+	req, _ := http.NewRequest(http.MethodPost, "/v2/vehicle", bytes.NewBuffer(marshal))
+	rr := httptest.NewRecorder()
+	// Act
+	router.ServeHTTP(rr, req)
+	//Assert
+	assert.Equal(t, expectedStatus, rr.Code)
+	var errorResponse lib.ErrorResponse
+	err = json.Unmarshal(rr.Body.Bytes(), &errorResponse)
+	require.NoError(t, err)
+	assert.Equal(t, expectedError, errorResponse)
+}
+
