d94737577286a525e9da24d33d5e039b8808ec3f
Mohammad Dehghanpour <m.dehghanpour10@gmail.com>
2021-09-25 14:58:44 +0330

    add createFacility service

---
+++ service/facilityService/create.go
@@ -0,0 +1,28 @@
+package facilityService
+
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"spotHeroProject/lib"
+	"spotHeroProject/models"
+)
+
+func (s *FacilityService) createFacilityService(facility models.Facility) error{
+	item, err := dynamodbattribute.MarshalMap(facility)
+	if err != nil {
+		fmt.Print("facilityService.Create - marshalMap - ")
+		return fmt.Errorf("%w - %v", lib.ErrInternal, err)
+	}
+	input := &dynamodb.PutItemInput{
+		Item:      item,
+		TableName: aws.String("FacilitySpot"),
+	}
+	_, err = s.db.PutItem(input)
+	if err != nil {
+		fmt.Printf("facilityService.Create - putItem - ")
+		return fmt.Errorf("%w - %v", lib.ErrInternal, err)
+	}
+	return nil
+}
