590f9b5a23d4799b7c77057a0d4b2ca1a1b01c5b
Mohammad Dehghanpour <m.dehghanpour10@gmail.com>
2021-09-25 14:00:02 +0330

    add delete reservation service

---
+++ service/reservationService/delete.go
@@ -0,0 +1,25 @@
+package reservationService
+
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"spotHeroProject/lib"
+)
+
+func (s *ReservationService) DeleteReservationService(reservationId string) error {
+	deleteItemInput := &dynamodb.DeleteItemInput{
+		TableName: aws.String("ReservationSpot"),
+		Key: map[string]*dynamodb.AttributeValue{
+			"reservation_id": {
+				S: aws.String(reservationId),
+			},
+		},
+	}
+	_, err := s.db.DeleteItem(deleteItemInput)
+	if err != nil {
+		fmt.Print("reservationService.delete - deleteItem - ")
+		return fmt.Errorf("%w - %v", lib.ErrInternal, err)
+	}
+	return nil
+}
