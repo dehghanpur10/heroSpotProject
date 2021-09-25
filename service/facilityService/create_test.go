513c2f09fd77a2b8aeca871851d16b6d7294a7e6
Mohammad Dehghanpour <m.dehghanpour10@gmail.com>
2021-09-25 15:07:30 +0330

    add unit test for create facility servicec

---
+++ service/facilityService/create_test.go
@@ -0,0 +1,37 @@
+package facilityService
+
+import (
+	"errors"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"spotHeroProject/mocks"
+	"spotHeroProject/models"
+	"testing"
+)
+
+func TestCreateFacilityService(t *testing.T) {
+	tests := []struct{
+		name string
+		putItemError error
+	}{
+		{name: "ok"},
+		{name: "putItem error",putItemError: errors.New("putItem error")},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			// Arrange
+			mockDB := new(mocks.DynamoDBAPI)
+			service := New(mockDB)
+			mockDB.On("PutItem",mock.Anything).Return(&dynamodb.PutItemOutput{},test.putItemError)
+			// Act
+			err := service.createFacilityService(models.Facility{})
+			// Assert
+			if err != nil {
+				assert.Contains(t, err.Error(),test.putItemError.Error())
+			} else {
+				assert.Nil(t, test.putItemError)
+			}
+		})
+	}
+}
