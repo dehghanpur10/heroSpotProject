d94d52b1f99952d73c1fe2ab287b3b351b1fdc7a
Mohammad Dehghanpour <m.dehghanpour10@gmail.com>
2021-09-25 15:13:48 +0330

    add delete facility service

---
+++ service/facilityService/delete.go
@@ -0,0 +1,24 @@
+package facilityService
+
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"spotHeroProject/lib"
+)
+
+func (s *FacilityService) delete(facilityId string) error {
+	deleteItemInput := &dynamodb.DeleteItemInput{
+		Key: map[string]*dynamodb.AttributeValue{
+			"facility_id": {
+				S: aws.String(facilityId),
+			},
+		},
+	}
+	_, err := s.db.DeleteItem(deleteItemInput)
+	if err != nil {
+		fmt.Print("facilityService.delete - deleteItem - ")
+		return fmt.Errorf("%w - %v", lib.ErrInternal, err)
+	}
+	return nil
+}
